7d6883ab30ace9eea3b4669c45f4b515ee67177a
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-22 11:14:50 -0700

    working in class

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ app.get('/', function(req, res){
 
 app.get('/search', function(req, res){
   // res.send("search page: " + query);
-  var query = req.query.searchTerm;
-  var searchURL = "http://www.omdbapi.com/?s=" + query;
+  var searchRequest = req.query.searchTerm;
+  var searchURL = "http://www.omdbapi.com/?s=" + searchRequest;
   request(searchURL, function(error, response, body){
-    res.send ("response received");
-  })
+    if (!error){
+      // res.send ("response received" + body);  
+      // var data = body;
+      // res.render("results.ejs", {movieList: data});  
+      var data = JSON.parse(body); 
+      res.render("results.ejs", {movieList: data.Search});
+      // res.send(data);
+    }
+
+  });
 });
 
 app.listen(3000);
